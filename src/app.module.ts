5a9f03faca4ca0c88f2b09c1eac80053325d71df
Délio Marcos <deliomarcos.contato@gmail.com>
2022-02-23 14:12:02 -0300

    Aplicando middleware de exemplo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
+import { logger } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+      consumer
+        .apply(logger)
+        .exclude(
+          { path: 'cats', method: RequestMethod.PUT },
+          { path: 'cats', method: RequestMethod.POST },
+          'cats/(.*)',
+        )
+        .forRoutes(CatsController);
+  }
+}
